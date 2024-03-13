81a7d8856ea5443e6adb0f85282aff7eaf58074f
Robin Karlin <51800173+rpkarlin@users.noreply.github.com>
2024-03-12 20:51:55 -0500

    tweak for needing to access not timitate data from the server

--- utils/get_exptLoadPath.m
+++ utils/get_exptLoadPath.m
@@ -12,6 +12,12 @@ if ispc
         if strcmp(computerName, 'WHITTAKER')
             % Dumb workaround for Whittaker
             basePath = ['D:\Users\' username '\OneDrive - University of Missouri\nesstlab\experiments\']; 
+        elseif strcmp(computerName, 'PERTWEE')  % This is a hack for working sometimes on server and sometimes on nesst
+            if ~strcmp(exptName, 'timitate') && ~strcmp(exptName, 'participant_database')
+                basePath = '\\wcs-cifs.waisman.wisc.edu\wc\smng\experiments\';
+            else
+                basePath = ['C:\Users\' username '\OneDrive - University of Missouri\nesstlab\experiments\']; 
+            end
         else
             basePath = ['C:\Users\' username '\OneDrive - University of Missouri\nesstlab\experiments\']; 
         end
