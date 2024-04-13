d7b5843d4914ca4447cee861c3a2bba99c8ece4a
Robin Karlin <51800173+rpkarlin@users.noreply.github.com>
2024-04-12 19:36:19 -0500

    script to install packages for new computers

---
+++ stats/install_labStatsPackages.R
@@ -0,0 +1,15 @@
+
+install.packages("ggplot2")
+
+install.packages("lmerTest")
+install.packages("lme4")
+install.packages("readr")
+install.packages("stringr")
+install.packages("gridExtra")
+install.packages("reshape2")
+install.packages("GGally")
+install.packages("emmeans")
+
+install.packages("ggtext")
+install.packages("glue")
+install.packages("dplyr")
